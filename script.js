7ebc9ddd9ba950642b02e3c746835043e7847579
Benedicte Naess <144252770+benedictenaess@users.noreply.github.com>
2023-10-18 13:17:16 +0200

    Display components

---
+++ script.js
@@ -0,0 +1,29 @@
+const overviewButtons = document.querySelectorAll('.overview__button');
+const components = document.querySelectorAll('.component');
+
+overviewButtons.forEach(overviewButton => {
+	const displayComponent = (event) => {
+		const button = event.currentTarget;
+		const componentDisplay = button.dataset.componentDisplay;
+	
+		overviewButtons.forEach(overviewButton => {
+			overviewButton.classList.remove('overview__button--active');
+
+			if (overviewButton.dataset.componentDisplay === componentDisplay) {
+				button.classList.add('overview__button--active');
+			}
+		})
+	
+		components.forEach(component => {
+			component.classList.remove('component--visible');
+	
+			const componentName = component.dataset.componentName;
+	
+			if (componentName === componentDisplay) {
+				component.classList.add('component--visible');
+			}
+		});
+	}
+
+	overviewButton.addEventListener('click', displayComponent);
+});
