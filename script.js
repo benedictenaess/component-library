e046668675964d48457c12856aea2b0b828236ff
Benedicte Naess <144252770+benedictenaess@users.noreply.github.com>
2023-10-19 15:08:27 +0200

    Select component js

--- script.js
+++ script.js
@@ -27,3 +27,22 @@ overviewButtons.forEach(overviewButton => {
 
 	overviewButton.addEventListener('click', displayComponent);
 });
+
+//Select component
+
+const selected = document.querySelector('.selected');
+const optionsContainer = document.querySelector('.options-container');
+
+const optionsList = document.querySelectorAll('.option');
+
+selected.addEventListener('click', () => {
+	optionsContainer.classList.toggle('active');
+})
+
+optionsList.forEach(option => {
+	option.addEventListener('click', () => {
+		selected.innerText = option.querySelector('label').innerText; 
+		optionsContainer.classList.remove('active');
+	})
+
+})
